22f8e47d82b8a5d2d0a7c4918ef1fb2d85c35b62
Loïc Payol <contact@loicpayol.fr>
2025-07-11 00:54:50 +0200

    feat: implement previous submissions with check for existing

---
+++ apps/web/src/hooks/useRiddleGame.ts
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+type RiddleGameState = {
+  riddle?: string;
+  contractAddress: string;
+} & (
+  | { status: 'waitingForRiddle' }
+  | { status: 'guess'; riddle: string }
+  | { status: 'solved'; riddle: string; winner: string; solution: string }
+)
+
+export function useRiddleGame() {
+  const state: RiddleGameState = {
+    contractAddress: '',
+    riddle: "what is the name of the only son of Bob's father?",
+    status: 'guess',
+  }
+
+  const [submittedSet, setSubmittedSet] = useState<Set<string>>(
+    () => new Set([])
+  );
+
+  const isSubmitted = (submission: string) => {
+    return Array.from(submittedSet).some((s) => s === submission);
+  };
+
+  const submit = (submission: string) => {
+    if (!isSubmitted(submission)) {
+      setSubmittedSet((prev) => new Set([...prev, submission]));
+    }
+  };
+
+  return {
+    status: state.status,
+    riddle: state.riddle,
+    submissions: Array.from(submittedSet),
+    submit,
+    isSubmitted,
+  };
+} 
