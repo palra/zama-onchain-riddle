68af13419ce19d74cca3fe04d77014e5f10bb957
Loïc Payol <contact@loicpayol.fr>
2025-07-14 17:45:00 +0200

    ci: fix deployment on Vercel

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['jotai-devtools'],
+  transpilePackages: ['jotai-devtools', '@repo/contracts'],
 };
 
 export default nextConfig;
